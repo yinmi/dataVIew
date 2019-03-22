86d41f459d83a685104b4e41fb98aa2b7d538d18
yinmi <chunhuang91@gmail.com>
2019-03-22 21:03:33 +0800

    自动计算

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 
 python exercise
-<<<<<<< HEAD
 dataView 数据可视化
-=======
-dataView 数据可视化
->>>>>>> 6079673baaa70af6c1bddae4ae99ee12b3d295ff
---
+++ scatter_square.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt 
+x_values=list(range(1,1001))
+y_values=[x**2 for x in x_values]
+plt.scatter(x_values,y_values,s=2)
+plt.title("Squarre Number", fontsize=24)
+plt.xlabel("value",fontsize=12)
+plt.ylabel("square of value",fontsize=14)
+plt.tick_params(axis='both',which='major',labelsize=14)
+
+plt.savefig('scaquartPlot.png',bbox_inches='tight')
+plt.show()
