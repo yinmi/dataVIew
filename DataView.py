e100da0f3d3aa58f183b15ae9954fe709a653f57
yinmi <chunhuang91@gmail.com>
2019-03-22 21:03:33 +0800

    15.2.1

--- DataView.py
+++ DataView.py
@@ -1,1 +1,8 @@
-import
+import matplotlib.pyplot as plt
+
+squares=[1,4,9,16,25]
+plt.plot(squares,linewidth=5)
+plt.xlabel("Value",fontsize=14)
+plt.ylabel("Square of Value",fontsize=14)
+plt.tick_params(axis='both',labelsize=14)
+plt.show()
